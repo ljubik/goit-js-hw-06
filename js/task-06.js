2772e88fe0fa085571d4fb6f2e2ad88571b787a0
Liubomyr <ljubik@ukr.net>
2020-11-05 13:00:23 +0200

    task #06 complete

--- js/task-06.js
+++ js/task-06.js
@@ -1,126 +1,140 @@
 // Завдання 6
-// Задача 3-6
-// Суммирование значений свойства из массива объектов
-// Напиши функцию calculateTotalPrice(allProdcuts, productName),
-// которая получает массив объектов и имя продукта (значение свойства name).
-// Возвращает общую стоимость продукта (цена умноженная на количество).
+// Задача 6-6
 
-// Вызовы функции для проверки работоспособности твоей реализации.
+const users =  [
+  {
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
 
-// calculateTotalPrice(products, 'Радар'));
-// 9080
+// filter, map и создание анонимных объектов
+// Получи массив из объектов, которые состоят только из свойств name и email тех пользователей, 
+// которые попадают в возрастную категорию от min до max лет (значение свойства age).
 
-// calculateTotalPrice(products, 'Сканер')); // 10200
+// Сравнение, пример
 
-// calculateTotalPrice(products, 'Захват')); // 2400
+// const x = 13;
 
-// calculateTotalPrice(products, 'Дроид')); // 2800
+// // num больше 10 ?
+// const larger = num > 10; // true
+
+// // num меньше 100 ?
+// const smaller = 100 > num; // true
+
+// // num находитя между 10 и 100 ?
+// const between = smaller && larger; // true
+// Создание анонимных объектов:
+
+// const arr = Array(3)
+//   .fill('')
+//   .map((e,i) => ({['index']: i }));
+// /* [ { index: 0 }, { index: 1 }, { index: 2 } ] */
+// Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив.
+//  Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
+
 
 // Здано:
-// function calculateTotalPrice(array, prop) {
-// 	"use strict"
-// 	// Write code under this line
-// let total = 0;
+// Write code under this line
+// const getUsersWithAge = (array, min, max) => array
+//  .filter(({age}) => age)   
+//  .map(({name, email}) => ({}));  
 
-// for (const item of array){
-// 	if (item.name === prop){
-// 		total+= item.price * item.quantity;
-// 	}
-// }
-// return total
+const getUsersWithAge = (array, min, max) => array
+ .filter(({age}) => age >= min && age <= max)   
+ .map(({name, email}) => ({name, email}));  
 
+ console.log(getUsersWithAge(users, 20, 30));
+/* [
+    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
+    { name: 'Elma Head', email: 'elmahead@omatom.com' },
+    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
+] */
 
-// // for (const arr of array){
-	
-// // 	const item = Object.entries(arr);
-// // 	let sum = 0;
-  
-// // 	for (const basketEntry of item) {
-// // 	  const [name, count] = basketEntry;
-// // 	  const priceFromYear = prop[name];
-  
-// // 	  sum += priceFromYear * count;
-// // 	}
-  
-
-// // // console.log("total++",total)
-// // // 	for (const key in arr) {
-// // // 		total = arr.price * arr.quantity;
-// // // 		// console.log("key", key)
-// // // 		// total += arr.price * arr.quantity;
-// // // 		// if (arr.name === prop){
-// // // 		// 	total = arr.price * arr.quantity;
-// // // 		// 	// console.log("arr.name", arr.name)
-// // // 		// }
-		
-// // // 	// total += arr.price * arr.quantity
-// // // }
-// // // total+=total
-
-// // return total;
-// // }
-
-// // console.log("total", total)
-// }
-
-// // Объекты и ожидаемый результат
-// const products = [
-// 	{ name: "Радар", price: 1300, quantity: 4 },
-// 	{ name: "Радар", price: 1280, quantity: 2 },
-// 	{ name: "Радар", price: 1320, quantity: 1 },
-// 	{ name: "Сканер", price: 2700, quantity: 1 },
-// 	{ name: "Сканер", price: 2500, quantity: 3 },
-// 	{ name: "Дроид", price: 400, quantity: 7 },
-// 	{ name: "Захват", price: 1200, quantity: 2 },
-// ]
-
-// console.log(calculateTotalPrice(products, "Радар"))
-// 9080
-
-// console.log(calculateTotalPrice(products, "Сканер"))
-// // 10200
-
-// console.log(calculateTotalPrice(products, "Захват"))
-// // 2400
-
-// console.log(calculateTotalPrice(products, "Дроид"))
-// 2800
-
-
-
-// Здано:
-function calculateTotalPrice (array, prop) {
-	'use strict';
-	// Write code under this line
-  let total = 0;
-  
-  for (const item of array){
-	  if (item.name === prop){
-		  total+= item.price * item.quantity;
-	  }
-  }
-  return total
-  }
-  
-  // Объекты и ожидаемый результат
-  const products = [
-	  { name: 'Радар', price: 1300, quantity: 4 },
-	  { name: 'Радар', price: 1280, quantity: 2 },
-	  { name: 'Радар', price: 1320, quantity: 1 },
-	  { name: 'Сканер', price: 2700, quantity: 1 },
-	  { name: 'Сканер', price: 2500, quantity: 3 },
-	  { name: 'Дроид', price: 400, quantity: 7 },
-	  { name: 'Захват', price: 1200, quantity: 2 }
-  ]; 
-  
-  console.log(calculateTotalPrice(products, 'Радар'));  
-  // 9080
-  
-  console.log(calculateTotalPrice(products, 'Сканер')); 
-  // 10200
-  
-  console.log(calculateTotalPrice(products, 'Захват'));
-  // 2400
-  
-  console.log(calculateTotalPrice(products, 'Дроид')); 
-  // 2800
+ console.log(getUsersWithAge(users, 30, 40));
+/* [
+    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
+    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
+    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
+    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
+] */
