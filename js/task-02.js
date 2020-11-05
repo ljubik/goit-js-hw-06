afd9be95aceda9a1dfa6e233a879434550da0bd9
Liubomyr <ljubik@ukr.net>
2020-11-05 11:41:15 +0200

    Task #02 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,21 +1,101 @@
-// Завдання 2
-// Задача 3-2
-// Подсчет количества свойств в объекте
-// Напиши функцию countProps(obj), которая возвращает число - количество свойств в объекте.
+// // Завдання 2
+// Задача 6-2
+// filter
+// Получи массив объектов пользователей, отобранный по цвету глаз (свойство eyeColor), 
+// используя деструктурирующее присваивание для параметра функции ({eyeColor})
+//  без пробелов и переносов на новую строку.
 
-// Циклы не должны использоваться
+// Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. 
+// Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
+
+// Деструктурирующее присваивание для параметра функции
+// PS Деструктурирующее присваивание (ДП):
+
+// Объект как параметр без ДП
+// const object = {num : 2}
+// function getNum (obj) { return obj.num; }
+// console.log(getNum(object)) // 2
+// ДП
+// const object = {num : 2}
+// // const num  =  object.num;
+// const { num } = object;
+// console.log(num) // 2
+// Объект как параметр c ДП
+// const object = {num : 2}
+// //function getNum (obj) { return obj.num; }
+// function getNum ({num}) { return num; }
+// console.log(getNum(object)) // 2
 
 // Здано:
-const countProps = function (obj) {
-	"use strict"
-	// Write code under this line
-	return Object.keys(obj).length
-}
+// Write code under this line
+// const getUsersWithEyeColor = (array, color) => array;
+const getUsersWithEyeColor = (array, color) => array.filter(({eyeColor}) => eyeColor === color);
 
-console.log(countProps({})) // 0
+console.log(getUsersWithEyeColor(users, 'blue'));
+/* [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  }
+] */
 
-console.log(countProps({ a: 1, b: 1 })) // 2
 
-console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })) // 5
 
-// https://learn.javascript.ru/keys-values-entries
+// Классная шпаргалка>
+// Шпаргалка по методам массива:
+// Для добавления/удаления элементов:
+// push (...items) – добавляет элементы в конец,
+// pop() – извлекает элемент с конца,
+// shift() – извлекает элемент с начала,
+// unshift(...items) – добавляет элементы в начало.
+// splice(pos, deleteCount, ...items) – начиная с индекса pos, удаляет deleteCount элементов и вставляет items.
+// slice(start, end) – создаёт новый массив, копируя в него элементы с позиции start до end (не включая end).
+// concat(...items) – возвращает новый массив: копирует все члены текущего массива и добавляет к нему items. Если какой-то из items является массивом, тогда берутся его элементы.
+// Для поиска среди элементов:
+// indexOf/lastIndexOf(item, pos) – ищет item, начиная с позиции pos, и возвращает его индекс или -1, если ничего не найдено.
+// includes(value) – возвращает true, если в массиве имеется элемент value, в противном случае false.
+// find/filter(func) – фильтрует элементы через функцию и отдаёт первое/все значения, при прохождении которых через функцию возвращается true.
+// findIndex похож на find, но возвращает индекс вместо значения.
+// Для перебора элементов:
+// forEach(func) – вызывает func для каждого элемента. Ничего не возвращает.
+// Для преобразования массива:
+// map(func) – создаёт новый массив из результатов вызова func для каждого элемента.
+// sort(func) – сортирует массив «на месте», а потом возвращает его.
+// reverse() – «на месте» меняет порядок следования элементов на противоположный и возвращает изменённый массив.
+// split/join – преобразует строку в массив и обратно.
+// reduce(func, initial) – вычисляет одно значение на основе всего массива, вызывая func для каждого элемента и передавая промежуточный результат между вызовами.
+// Дополнительно:
+// Array.isArray(arr) проверяет, является ли arr массивом.
+// Обратите внимание, что методы sort, reverse и splice изменяют исходный массив.
